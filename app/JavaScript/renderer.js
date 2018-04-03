8028243058f017f30d27c78a184fc902c9eece3a
Lukas PC <lukas@lukashermansson.me>
2018-04-03 23:33:08 +0200

    Added rendering engine dropdown

--- app/JavaScript/renderer.js
+++ app/JavaScript/renderer.js
@@ -3,13 +3,19 @@ setInterval(onTimerTick, 1); // 33 milliseconds = ~ 30 frames per sec
 
 let isRunning = false;
 let escapestate = false;
+let colormode = 1;
 
 const button = document.getElementById('accept');
 
 button.addEventListener('click', clickEvent);
 
+const box = document.getElementById('box');
+
+box.addEventListener('change', boxlistener);
+
+
 function onTimerTick() {
-  if (isRunning && !escapestate) {
+  if (isRunning && !escapestate && colormode === 1) {
     document.getElementById('body').style.background = getRandomColor();
   }
 }
@@ -27,6 +33,18 @@ function clickEvent() {
 
   const elem = document.getElementById('warning');
   elem.parentNode.removeChild(elem);
+}
+function boxlistener() {
+  const select = document.getElementById('box');
+  switch (select.options[select.selectedIndex].value) {
+    case 'FullColor':
+      colormode = 1;
+      break;
+
+    default:
+      colormode = 0;
+      break;
+  }
 }
 
 document.addEventListener('keydown', (event) => {
