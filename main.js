6ae182dad939421b2b570606788125920b7a2ccd
Lukas PC <lukas@lukashermansson.me>
2018-04-03 10:19:08 +0200

    Added template

---
+++ main.js
@@ -0,0 +1,54 @@
+const electron = require('electron');
+// Module to control application life.
+const app = electron.app;
+//const Menu = electron.Menu;
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require('path');
+const url = require('url');
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 800, height: 600, webPreferences: {
+    nodeIntegration: true,
+  }})
+
+  //Menu.setApplicationMenu(null);
+
+  // and load the index.html of the app.
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  // Open the DevTools.
+  //mainWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+//when app has loaded
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  // OSX recreation
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
