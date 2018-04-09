60565811faa02091f6d0aad6d6fda07f3328105a
Lukas Hermansson <lukas@lukashermansson.me>
2018-04-09 14:57:19 +0200

    Fixed corssover declaration

--- app/JavaScript/renderingModules/CrossoverColor.js
+++ app/JavaScript/renderingModules/CrossoverColor.js
@@ -5,12 +5,12 @@ module.exports = class OneColor extends ColorModule {
     super('Crossover');
   }
 
-  draw(ctx) {
-    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  draw(ctx, runningtime) {
+    const imageData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-    for (let y = 0; y < canvas.height; ++y) {
-      for (let x = 0; x < canvas.width; ++x) {
-        let index = ((y * canvas.width) + x) * 4;
+    for (let y = 0; y < ctx.canvas.height; ++y) {
+      for (let x = 0; x < ctx.canvas.width; ++x) {
+        let index = ((y * ctx.canvas.width) + x) * 4;
 
         imageData.data[index] = (y * runningtime + runningtime) % 255; // red
         imageData.data[++index] = (y / x / runningtime + runningtime) % 255; // green
@@ -18,6 +18,6 @@ module.exports = class OneColor extends ColorModule {
       }
     }
     // Update image
-    ctx.putImageData(imageData, 0, 0, 0, 0, canvas.width, canvas.height);
+    ctx.putImageData(imageData, 0, 0, 0, 0, ctx.canvas.width, ctx.canvas.height);
   }
 };
