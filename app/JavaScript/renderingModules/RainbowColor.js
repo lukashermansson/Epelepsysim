530b834ca8dd2b81a873339bc8a04e75da4e296a
Lukas Hermansson <lukas@lukashermansson.me>
2018-04-09 14:56:21 +0200

    Fixed canvas pointer

--- app/JavaScript/renderingModules/RainbowColor.js
+++ app/JavaScript/renderingModules/RainbowColor.js
@@ -5,12 +5,12 @@ module.exports = class RainbowColor extends ColorModule {
     super('Rainbow');
   }
 
-  draw(ctx) {
+  draw(ctx, runningtime) {
     const imageData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-    for (let y = 0; y < canvas.height; ++y) {
-      for (let x = 0; x < canvas.width; ++x) {
-        let index = ((y * canvas.width) + x) * 4;
+    for (let y = 0; y < ctx.canvas.height; ++y) {
+      for (let x = 0; x < ctx.canvas.width; ++x) {
+        let index = ((y * ctx.canvas.width) + x) * 4;
 
         imageData.data[index] = 100; // red
         imageData.data[++index] = (y / x * runningtime + runningtime) % 255; // green
